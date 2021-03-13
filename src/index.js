4f0af1a02f26f0f01b261df881fe42b58e5647f0
Денис Чухліб <dchuhleb@icloud.com>
2021-03-13 18:46:45 +0200

    feat:task done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    let res = array[0];
+    array.forEach(function (item, i, array) {
+      if (res > array[i]) {
+        res = array[i];
+      }
+    });
+    return res;
+  }
+
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    let res = array[0];
+    array.forEach(function (item, i, array) {
+      if (res < array[i]) {
+        res = array[i];
+      }
+    });
+    return res;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+
+  if (array === undefined || array.length === 0) {
+    return 0;
+  } else {
+    let res = 0;
+    array.forEach(function (item, i, array) {
+
+      res += array[i];
+
+    });
+    return res / array.length;
+  }
 }
